875bec90b174cb1b869b1bba4fe23b3e7a5261d1
Pavan kumar <kumarpavan1309@gmail.com>
2018-08-31 20:04:47 +0530

    remove config

--- config/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Secret key for JWT signing and encryption
-  secret: 'helloworld#2017',
-  // Setting port for server
-  port: process.env.Port || 3000,
-  // Database connection information
-  database: 'mongodb://localhost:27017/dct-code'
-};
